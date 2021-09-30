2c9cf07d6f09d90506ab0e36166943e2941cac76
Anna Wilson <ajoyna01@gmail.com>
2021-09-30 14:22:13 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [parks, setParks] = useState([])
+
+  useEffect(() => {
+    fetch("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=jiubTrXhccHzfcEc6ihhjVV18MssQBvGoLrHNkQw")
+      .then((r) => r.json())
+      .then((data) => {
+        setParks(data.data);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> {parks.map((park) => park.fullName)}
+     
     </div>
+  
   );
 }
 
